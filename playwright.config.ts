e086f7f6c1aeebf5f0462f2f23319882ac0d7362
Aleksei Avitsuk <ky200782aav@gmail.com>
2025-12-09 23:48:49 +0100

    add web server start command

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,9 +43,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'cd ../spring-petclinic && mvnw spring-boot:run',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+    timeout: 20 * 1000,
+  },
 });
